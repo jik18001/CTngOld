616d66cddcb597895ea1efec531df4c410d0e133
jik18001 <99697637+jik18001@users.noreply.github.com>
2022-08-11 16:03:36 -0400

    update gossip script

--- gossiperTest.sh
+++ gossiperTest.sh
@@ -1,6 +1,3 @@
 #/bin/bash
 
-go run . gossiper \
-testData/gossiperNetworkTest/gossiper_pub_config.json \
-testData/gossiperNetworkTest/$1/gossiper_priv_config.json \
-testData/gossiperNetworkTest/$1/gossiperCrypto.json
+go run . gossiper ./testData/gossiperNetworkTest/gossiper_pub_config.json ./testData/gossiperNetworkTest/$1/gossiper_priv_config.json ./testData/gossiperNetworkTest/$1/gossiperCrypto.json
